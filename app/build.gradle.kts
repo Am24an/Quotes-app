a4cbe499cf6f127d23146e9eb065c643ff977ac4
ADMIN <shalenmj@gmail.com>
2026-03-24 10:37:11 +0530

    version bump to 4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.shalenmathew.quotesapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 38
-        versionName = "4.3"
+        versionCode = 39
+        versionName = "4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
